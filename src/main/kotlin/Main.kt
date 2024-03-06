cc1dbae59a16bd2ef3745ac734aaee3caa9c971f
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-06 05:34:15 +0300

    Added files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,50 @@
+//fun main(){
+//    println("Hello Beverly")
+//   var remainder = modulus(7,2)
+//    println(remainder)
+//    var result =sum(4,5,7,8)
+//    println(result)
+//    println("Crocheting")
+//}
+//    fun printhello(name: String){
+//        println(name)
+//    }
+//    fun modulus(num1:Int,num2:Int,):Int{
+//        var remainder= num1 % num2
+//        return remainder
+//    }
+//    fun sum(num1:Int,num2:Int,num3:Int,num4:Int,):Int{
+//        var result = num1 + num2 + num3 + num4
+//        return result
+//    }
+//    fun funnyFact(word : String){
+//        println(word)
+
+//    }
 fun main(){
-    println("Hello Beverly")
-   var remainder = modulus(7,2)
-    println(remainder)
-    var result =sum(4,5,7,8)
-    println(result)
-    println("Crocheting")
+    var name="Beverly"
+    name="Beverly"
+    val lastname="Akinyi"
+    val student="Akinyi"
+
+
+    var type="car"
+    type="car"
+
+    var y="food"
+
+    var food="rice"
+    print(food)
+
+    print(food)
+
+    print(food)
+    var fruit="mango"
+
+
 }
-    fun printhello(name: String){
-        println(name)
-    }
-    fun modulus(num1:Int,num2:Int,):Int{
-        var remainder= num1 % num2
-        return remainder
-    }
-    fun sum(num1:Int,num2:Int,num3:Int,num4:Int,):Int{
-        var result = num1 + num2 + num3 + num4
-        return result
-    }
-    fun funnyFact(word : String){
-        println(word)
-    }
 
-
-
-
+//
+//
+//
+//
